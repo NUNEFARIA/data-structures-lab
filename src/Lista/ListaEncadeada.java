c4ebbce686239947333500ec1b7820c874640fe7
NUNES <nunesfariasgustavo@gmail.com>
2026-04-22 19:52:08 -0300

    Implementado o tributo contador a classe Lista

--- src/Lista/ListaEncadeada.java
+++ src/Lista/ListaEncadeada.java
@@ -1,6 +1,26 @@
 package Lista;
 
-public class ListaEncadeada {
+public class ListaEncadeada<T> {
     
-    private No inicio;
+    private No<T> inicio; //
+    private int tamanho = 0;
+    
+    // Adiciona elementos a lista
+    public void  adiciona (T elemento) {
+        No<T> celula = new No<>(elemento);
+        this.inicio = celula;
+        this.tamanho++;
+    }
+    
+    // Retorna o tamanho da Lista
+    public int getTamanho() {
+        return this.tamanho;
+    }
+    
+    @Override
+    public String toString() {
+        return "ListaEncadeada{" +
+                "inicio=" + inicio +
+                '}';
+    }
 }
