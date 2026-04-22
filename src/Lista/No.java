6a4f5c8b03e08ee8be9681958339ca9e0f821eb7
NUNES <nunesfariasgustavo@gmail.com>
2026-04-22 18:48:37 -0300

    Documentada a classe No

--- src/Lista/No.java
+++ src/Lista/No.java
@@ -1,17 +1,19 @@
 package Lista;
 
-public class No<T> {
+/* Cada nó armazane um valor e o proximo item da lista*/
+public class No<T> { // # T neste caso é um tipo generico, qualquer
+    // outra letra ou palavra poderia ser usada em seu lugar
     
-    private T elemento;
-    private No<T> proximo;
+    private T elemento; // # Armazena o dado
+    private No<T> proximo; // # Aponta para o proximo nó da lista
     
     // Builders
-    public No(T elemento) {
+    public No(T elemento) { // # Instancia nó solitario
         this.elemento = elemento;
         this.proximo = null;
     }
     
-    public No(T elemento, No<T> proximo) {
+    public No(T elemento, No<T> proximo) { // Instancia nó que aponta para outro
         this.elemento = elemento;
         this.proximo = proximo;
     }
